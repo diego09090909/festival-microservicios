45cf3751a11717caf592ecdbe8817986665f3c65
Maika581 <jo.sanchezt@duocuc.cl>
2026-05-18 23:54:09 -0400

    arreglos client y errores

---
+++ src/main/java/com/festival/ms_usuario/dto/TokenResponseDTO.java
@@ -0,0 +1,21 @@
+package com.festival.ms_usuario.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+
+@Data
+@AllArgsConstructor
+public class TokenResponseDTO {
+
+    private String token;
+    private String tipo = "Bearer";
+    private String email;
+    private String rol;
+
+    public TokenResponseDTO(String token, String email, String rol) {
+        this.token = token;
+        this.email = email;
+        this.rol = rol;
+    }
+}
